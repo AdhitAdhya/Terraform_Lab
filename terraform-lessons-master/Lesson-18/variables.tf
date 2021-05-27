a0fbb6e179f213d1f3c8690f989bbfe3ec2f91cc
Ranger_Merah <77005527+AdhitAdhya@users.noreply.github.com>
2021-05-27 21:07:36 +0700

    Delete terraform-lessons-master directory

--- terraform-lessons-master/Lesson-18/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_users" {
-  description = "List of IAM Users to create"
-  default     = ["vasya", "petya", "kolya", "lena", "masha", "misha", "vova", "donald"]
-}
--- terraform-lessons-master/Lesson-28/new-prod/ip-prod.tf
+++
@@ -1,2 +0,0 @@
-resource "aws_eip" "prod-ip1" {}
-resource "aws_eip" "prod-ip2" {}
